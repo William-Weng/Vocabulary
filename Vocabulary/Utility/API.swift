e78714396d0d7cffd26bf782a73c8f19b504a4cb
William.Weng <linuxice0609@gmail.com>
2023-02-17 22:56:20 +0800

    - 將搜尋要猜的單字列表，取到三天前的資料

--- Vocabulary/Utility/API.swift
+++ Vocabulary/Utility/API.swift
@@ -117,19 +117,24 @@ extension API {
     /// 搜尋要猜的單字列表 (複習)
     /// - Parameters:
     ///   - level: 難度等級
+    ///   - days: 幾天前後的資料
     ///   - tableName: 資料表名稱
     ///   - count: 數量
     ///   - offset: 偏移量
     /// - Returns: [[String : Any]]
-    func searchGuessWordList(with level: Vocabulary.Level, for tableName: Constant.VoiceCode, count: Int = 10, offset: Int) -> [[String : Any]] {
+    func searchGuessWordList(with level: Vocabulary.Level, days: Int = -3, for tableName: Constant.VoiceCode, count: Int = 10, offset: Int) -> [[String : Any]] {
         
-        guard let database = Constant.database else { return [] }
+        guard let database = Constant.database,
+              let time = Date()._adding(component: .day, value: days)?._localTime()
+        else {
+            return []
+        }
         
-        let condition = SQLite3Condition.Where().isCompare(key: "level", type: .equal, value: level.rawValue)
+        let condition = SQLite3Condition.Where().isCompare(key: "level", type: .equal, value: level.rawValue).andCompare(key: "createTime", type: .lessThan, value: time)
         let limit = SQLite3Condition.Limit().build(count: count, offset: 0)
         let orderBy = SQLite3Condition.OrderBy().item(key: "level", type: .ascending).addItem(key: "review", type: .ascending).addItem(key: "createTime", type: .descending)
         let result = database.select(tableName: tableName.vocabularyList(), type: VocabularyList.self, where: condition, orderBy: orderBy, limit: limit)
-        
+                
         return result.array
     }
     
